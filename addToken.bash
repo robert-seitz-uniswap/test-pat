12a8c0409bd2abd7c6f83b1e10bd866bb2a28b37
Robert Seitz <robert.seitz@uniswap.org>
2024-12-06 11:49:37 -0500

    support multiple chain updates

--- addToken.bash
+++ addToken.bash
@@ -28,13 +28,13 @@ esac
 folderName=${repository#*Uniswap/}
 folderName=${folderName%.git*}
 
-if [ ! -d /path/to/directory ];
+if [ ! -d node_modules ];
 then
   echo 'installing dependencies'
   npm install
 fi
 
-if [ ! -d /path/to/directory ];
+if [ ! -d $repository ];
 then
  echo 'cloning'
  git clone $repository
@@ -43,16 +43,32 @@ fi
 cd ./$folderName/src/tokens
 git reset --hard && git checkout main && git pull
 files=($(ls -1 | sed -e 's/\..*$//'))
-getChoice -q "Select Chain" -o files -v "selectedChain"
 
-chainName=$(getChainIdFromName $selectedChain)
-chainId=$(getChainNameFromId $chainName)
-getJsonList jsonList names $chainId
-cat "./$selectedChain.json" | jq ". += $jsonList" > newfile
-mv newfile "./$selectedChain.json"
+addAnotherChain="Yes"
+allNames=()
+names=()
+while [ "$addAnotherChain" == "Yes" ]
+do
+  getChoice -q "Select Chain" -o files -v "selectedChain"
+  chainName=$(getChainIdFromName $selectedChain)
+  chainId=$(getChainNameFromId $chainName)
+  getJsonList jsonList names $chainId
+  cat "./$selectedChain.json" | jq ". += $jsonList" > newfile
+  mv newfile "./$selectedChain.json"
+  allNames+=("${names[@]}")
+  # if [ ${#allNames[@]} -eq 0 ]; then
+  #   echo "No errors, hooray"
+  # else
+  #   echo "Oops, something went wrong..."
+  # fi
+
+  echo $allNames
+  choices=("No" "Yes")
+  getChoice -q "Modify Another Chain?" -o choices -v "addAnotherChain"
+done
 
 random=$((1 + $RANDOM % 100))
-branchName=add-$(printf '%s-' "${names[@]}")$(date '+%Y-%m-%d')-$random
+branchName=add-$(printf '%s-' "${allNames[@]}")$(date '+%Y-%m-%d')-$random
 branchName="${branchName// /_}"
 
 git checkout -b $branchName
