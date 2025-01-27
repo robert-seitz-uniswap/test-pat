25251ea95c4f22b48efd359d9cb7ce2f013e5bf7
Robert Seitz <robert.seitz@uniswap.org>
2025-01-27 16:16:54 -0500

    add js implementations

---
+++ getTokenFromApi.js
@@ -0,0 +1,48 @@
+import { ethers } from "ethers";
+
+async function getTokenFromApi(chain, address) {
+  // Get checksummed address (assuming the checksum function is imported)
+  const checksummedAddress = getChecksumAddress(address);
+
+  // Construct the GraphQL query
+  const query = {
+    query: `query Token($chain: Chain!, $address: String = null) {
+            token(chain: $chain, address: $address) {
+                address
+                symbol
+                decimals
+                name
+            }
+        }`,
+    variables: {
+      chain: chain,
+      address: checksummedAddress,
+    },
+  };
+
+  // Make the API request
+  const response = await fetch("https://api.uniswap.org/v1/graphql", {
+    method: "POST",
+    headers: {
+      accept: "*/*",
+      "content-type": "application/json",
+      origin: "https://app.uniswap.org",
+    },
+    body: JSON.stringify(query),
+  });
+
+  const data = await response.json();
+  return data.data.token;
+}
+
+const getChecksumAddress = (address) => {
+  try {
+    const checksummed = ethers.getAddress(address);
+    return checksummed;
+  } catch (e) {
+    process.stderr.write(e);
+    throw e;
+  }
+};
+
+await getTokenFromApi("ETHEREUM", "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48");
---
+++ menu.js
@@ -0,0 +1,145 @@
+// menu.js
+import { emitKeypressEvents, cursorTo, clearScreenDown } from "readline";
+import chalk from "chalk";
+class Menu {
+  constructor() {
+    this.currentSelection = "";
+
+    // Configure readline
+    emitKeypressEvents(process.stdin);
+    process.stdin.setRawMode(true);
+  }
+
+  hideCursor() {
+    process.stdout.write("\x1B[?25l");
+
+    // Handle CTRL+C
+    process.on("SIGINT", () => {
+      this.showCursor();
+      console.log("");
+      process.exit(0);
+    });
+  }
+
+  showCursor() {
+    process.stdout.write("\x1B[?25h");
+  }
+
+  renderMenu(instruction, items, selectedIndex, maxViewable = 0) {
+    let start = 0;
+    let listLength = items.length;
+    let longest = Math.max(...items.map((item) => item.length));
+    let menuStr = `\n ${instruction}\n`;
+
+    // Handle scrolling view if maxViewable is set
+    if (maxViewable !== 0) {
+      listLength = Math.min(maxViewable, items.length);
+      if (selectedIndex >= listLength) {
+        start = selectedIndex + 1 - listLength;
+        listLength = selectedIndex + 1;
+      }
+    }
+
+    // Render menu items
+    for (let i = start; i < listLength; i++) {
+      const currItem = items[i];
+      const spaces = " ".repeat(longest - currItem.length);
+
+      if (i === selectedIndex) {
+        this.currentSelection = currItem;
+        menuStr += `\n ${chalk.green("ᐅ")} ${chalk.green(currItem)}${spaces}`;
+      } else {
+        menuStr += `\n   ${currItem}${spaces}`;
+      }
+    }
+
+    menuStr += "\n";
+
+    // Clear previous menu and render new one
+    cursorTo(process.stdout, 0, 0);
+    clearScreenDown(process.stdout);
+    process.stdout.write(menuStr);
+  }
+
+  async getChoice(options) {
+    const {
+      query = "Select an item from the list:",
+      items = [],
+      initialIndex = 0,
+      maxViewable = 0,
+    } = options;
+
+    if (!items.length) {
+      console.error(chalk.red("\n [ERROR] No menu items provided"));
+      return null;
+    }
+
+    let selectedIndex = initialIndex;
+    this.hideCursor();
+    // Re-enable raw mode for each menu instance
+    process.stdin.setRawMode(true);
+
+    return new Promise((resolve) => {
+      const handleKeypress = (str, key) => {
+        if (key.name === "c" && key.ctrl) {
+          this.showCursor();
+          process.exit();
+        }
+
+        switch (key.name) {
+          case "up":
+            selectedIndex = (selectedIndex - 1 + items.length) % items.length;
+            this.renderMenu(query, items, selectedIndex, maxViewable);
+            break;
+
+          case "down":
+            selectedIndex = (selectedIndex + 1) % items.length;
+            this.renderMenu(query, items, selectedIndex, maxViewable);
+            break;
+
+          case "return":
+            this.showCursor();
+            process.stdin.removeListener("keypress", handleKeypress);
+            process.stdin.setRawMode(false);
+            resolve(this.currentSelection);
+            break;
+        }
+      };
+
+      process.stdin.on("keypress", handleKeypress);
+      this.renderMenu(query, items, selectedIndex, maxViewable);
+    });
+  }
+}
+
+// Example usage:
+async function example() {
+  const menu = new Menu();
+  const foodOptions = [
+    "pizza",
+    "burgers",
+    "chinese",
+    "sushi",
+    "thai",
+    "italian",
+  ];
+
+  const firstChoice = await menu.getChoice({
+    query: "What do you feel like eating?",
+    items: foodOptions,
+    initialIndex: 0,
+    maxViewable: 4,
+  });
+
+  const secondChoice = await menu.getChoice({
+    query: "Select another option in case the first isn't available",
+    items: foodOptions,
+    initialIndex: 0,
+    maxViewable: 4,
+  });
+  console.log(`\n First choice is '${firstChoice}'`);
+
+  console.log(`\n Second choice is '${secondChoice}'`);
+}
+
+export default Menu;
